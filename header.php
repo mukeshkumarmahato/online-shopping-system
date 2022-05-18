8626c2e7a3f8d637ed5cb12b18ed6c139aee6ca8
mukeshkumarmahato <mukeshmahato220@gmail.com>
2022-05-18 16:45:50 +0545

    initial commit

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Online Shopping</title>
+	<title>Online Shopping System</title>
 	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
 	<!-- Font Awesome Icon -->
